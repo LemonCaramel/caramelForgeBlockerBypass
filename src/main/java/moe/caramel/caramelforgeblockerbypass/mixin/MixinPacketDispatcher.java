d1ea19f9fd7368385a6631f88ee1a0a9d5056ed9
LemonCaramel <lemoncaramel_@naver.com>
2020-12-29 13:53:08 +0900

    Add FML2 Handshake Detect Bypass

---
+++ src/main/java/moe/caramel/caramelforgeblockerbypass/mixin/MixinPacketDispatcher.java
@@ -0,0 +1,28 @@
+package moe.caramel.caramelforgeblockerbypass.mixin;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.PacketDispatcher;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.function.BiConsumer;
+
+@Mixin(PacketDispatcher.class)
+public abstract class MixinPacketDispatcher {
+
+    @Shadow(remap = false)
+    BiConsumer<ResourceLocation, PacketBuffer> packetSink;
+
+    /**
+     * @author LemonCaramel
+     * @reason Bypass FML2 Detect
+     */
+    @Overwrite(remap = false)
+    public void sendPacket(ResourceLocation resourceLocation, PacketBuffer buffer) {
+        if (!resourceLocation.getNamespace().equals("fml"))
+            packetSink.accept(resourceLocation, buffer);
+    }
+
+}
