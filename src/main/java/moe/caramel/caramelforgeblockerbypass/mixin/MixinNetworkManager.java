d5fe02487fcab1c5ad9104cc7c6c1e5c6d7c10e4
LemonCaramel <lemoncaramel_@naver.com>
2021-02-26 13:44:22 +0900

    Fixed an issue that might not send a brand name to the server

--- src/main/java/moe/caramel/caramelforgeblockerbypass/mixin/MixinNetworkManager.java
+++ src/main/java/moe/caramel/caramelforgeblockerbypass/mixin/MixinNetworkManager.java
@@ -1,19 +1,21 @@
 package moe.caramel.caramelforgeblockerbypass.mixin;
 
 import io.netty.buffer.Unpooled;
+import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.IPacket;
 import net.minecraft.network.play.client.CCustomPayloadPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
-
 @Mixin(NetworkManager.class)
 public abstract class MixinNetworkManager {
+
+    @Shadow public abstract INetHandler getNetHandler();
 
     public MixinNetworkManager() {
     }
@@ -26,13 +28,10 @@ public abstract class MixinNetworkManager {
     private void sendPacket(IPacket<?> packet, CallbackInfo callbackInfo) {
         if (packet instanceof CCustomPayloadPacket) {
             CCustomPayloadPacket customPayloadPacket = (CCustomPayloadPacket) packet;
-            String channelName = customPayloadPacket.getName().toString();
-            if (channelName.equalsIgnoreCase("minecraft:brand")) {
-                try {
-                    customPayloadPacket.readPacketData((new PacketBuffer(Unpooled.buffer())).writeString("vanilla"));
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                }
+            if (customPayloadPacket.getName() == CCustomPayloadPacket.BRAND) {
+                getNetHandler().getNetworkManager().sendPacket(new CCustomPayloadPacket(CCustomPayloadPacket.BRAND,
+                        (new PacketBuffer(Unpooled.buffer())).writeString("vanilla")), null);
+                callbackInfo.cancel();
             }
         }
     }
